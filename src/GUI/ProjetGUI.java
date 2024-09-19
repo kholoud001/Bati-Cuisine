c3b93b0850c3d80f7c2ba20dc325907608f7c638
kholoud001 <kholoud.sanak@gmail.com>
2024-09-19 12:32:47 +0100

    BC-1 update projet attrributes

--- src/GUI/ProjetGUI.java
+++ src/GUI/ProjetGUI.java
@@ -21,23 +21,50 @@ public class ProjetGUI {
     }
 
     public void addProjet(Client client) throws SQLException {
-        System.out.println("***** Add Project ******");
-        System.out.println("Enter projet name: ");
-        String nomProjet=scanner.nextLine();
+        System.out.println("--- Création d'un Nouveau Projet ---");
+        System.out.print("Entrez le nom du projet: ");
+        String nomProjet = scanner.nextLine();
 
-        System.out.print("Enter profit margin: ");
-        double margeBeneficiaire = scanner.nextDouble();
+        System.out.print("Entrez la surface de la cuisine (en m²) : ");
+        double surface = scanner.nextDouble();
+        scanner.nextLine();
 
-        System.out.print("Enter total cost: ");
-        double coutTotal = scanner.nextDouble();
 
+        // Project TVA
+        System.out.println("Souhaitez-vous appliquer une TVA au projet ? (y/n) : ");
+        String response = scanner.nextLine().toLowerCase();
+        double tvaProjet = 0;
+
+        if (response.equals("y")) {
+            System.out.println("Entrez le pourcentage de TVA (%) :  ");
+            tvaProjet = scanner.nextDouble();
+            scanner.nextLine();
+        }else{
+            tvaProjet = 0;
+        }
+
+        // Marge
+        System.out.println("Souhaitez-vous appliquer une marge bénéficiaire au projet ? (y/n) : ");
+        String answer= scanner.nextLine().toLowerCase();
+        double margeBeneficiaire=0;
+        if(answer.equals("y")) {
+            System.out.print("Entrez le pourcentage de marge bénéficiaire (%) : ");
+            margeBeneficiaire = scanner.nextDouble();
+            scanner.nextLine();
+        }
+
+
+        System.out.println("Calcul du coût en cours...");
+       // System.out.print("Enter total cost: ");
+        //double coutTotal = scanner.nextDouble();
+        double coutTotal=0;
         scanner.nextLine();
 
         System.out.println("Enter project state (EN_COURS, TERMINE, ANNULE): ");
         String etat = scanner.nextLine();
         EtatProjet etatProjet = EtatProjet.valueOf(etat.toUpperCase());
 
-        Projet projet = new Projet(nomProjet, margeBeneficiaire, coutTotal, etatProjet, client);
+        Projet projet = new Projet(nomProjet, surface, tvaProjet, margeBeneficiaire, coutTotal, etatProjet, client);
         projetService.createProject(projet);
         System.out.println("Project added successfully.");
 
